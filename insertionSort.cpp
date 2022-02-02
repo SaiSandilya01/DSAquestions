ae2aa3a2beefbf66791759dcb93f52000ddd67af
Sandilya <sai.ventrapragada.btech2019@sitpune.edu.in>
2022-02-02 08:58:52 +0530

    added some explanation and properties

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,3 +1,11 @@
+// Time Complexity: O(n^2) 
+// Auxiliary Space: O(1)
+// Boundary Cases: Insertion sort takes maximum time to sort if elements are sorted in reverse order. And it takes minimum time (Order of n) when elements are already sorted.
+// Algorithmic Paradigm: Incremental Approach
+// Sorting In Place: Yes
+// Stable: Yes
+// Online: Yes
+// Uses: Insertion sort is used when number of elements is small. It can also be useful when input array is almost sorted, only few elements are misplaced in complete big array.
 #include<bits/stdc++.h>
 
 using namespace std; 
@@ -29,7 +37,7 @@ void solve(vector<int> &arr){
 
 int main(){
   
-  vector<int> arr = {5,4,3,2,1};
+  vector<int> arr = {5,4,3,2,1,10, 3, 2, 1,5, 11};
 
   solve(arr);
 
